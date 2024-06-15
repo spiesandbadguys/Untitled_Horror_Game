6c88bf7d1155fe045673ea8c906a5ae092a7c2dc
Jonathan George <jongeorge1999@gmail.com>
2024-06-15 15:49:17 -0400

    Key pickups working

--- Scripts/pickup.gd
+++ Scripts/pickup.gd
@@ -1,6 +1,9 @@
-extends Node3D
+extends RigidBody3D
 
+@export var slot_data: SlotData
+@onready var mesh_3d: MeshInstance3D = $mesh
 var isIn = false
+var body = null
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,17 +14,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if isIn and Input.is_action_just_pressed("interact") and not %Player.getIsInMenu():
-		%Player.addKey()
-		queue_free()
+		# %Player.addKey()
+		if body.inventory_data.pick_up_slot_data(slot_data):
+			queue_free()
 
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player"):
 		isIn = true
+		self.body = body
 		print("in")
 
 
 func _on_area_3d_body_exited(body):
 	if body.is_in_group("Player"):
 		isIn = false
+		self.body = body
 		print("out")
